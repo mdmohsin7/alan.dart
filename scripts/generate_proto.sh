c9a54b4c1f0504c549158ecd5acd9062894483da
mdmohsin7 <mdmohsin_1@hotmail.com>
2022-07-16 15:43:52 +0530

    update get_proto for sdk 0.45.6 and cosmwasm

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 OUT=lib/proto
 PROTO=proto
 THIRD_PARTY=third_party
-COSMOS_VERSION=0.42.5
+COSMOS_VERSION=0.45.6
 
 ## Download the Protobuf files
 source scripts/get_proto.sh $PROTO $THIRD_PARTY $OUT $COSMOS_VERSION
@@ -22,7 +22,7 @@ done
 proto_dirs=$(find "$PROTO" -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   $PROTOC -I$PROTO \
-  --gocosmos_out=plugins=interfacetype+grpc,\
+  --gofast_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
